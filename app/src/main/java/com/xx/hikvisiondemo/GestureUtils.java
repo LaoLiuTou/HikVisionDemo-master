c5b1971ac10b1650f1052d7a634eb023e68034a0
TongTong <369259353@qq.com>
2019-04-02 09:12:13 +0800

    Initial commit

---
+++ app/src/main/java/com/xx/hikvisiondemo/GestureUtils.java
@@ -0,0 +1,40 @@
+package com.xx.hikvisiondemo;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+/**
+ * Created by Administrator on 2018/12/5.
+ */
+
+public class GestureUtils {
+    //获取屏幕的大小
+    public static Screen getScreenPix(Context context) {
+        DisplayMetrics dm = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(dm);
+        return new Screen(dm.widthPixels,dm.heightPixels);
+    }
+
+    public static class Screen{
+
+        public int widthPixels;
+        public int heightPixels;
+
+        public Screen(){
+
+        }
+
+        public Screen(int widthPixels,int heightPixels){
+            this.widthPixels=widthPixels;
+            this.heightPixels=heightPixels;
+        }
+
+        @Override
+        public String toString() {
+            return "("+widthPixels+","+heightPixels+")";
+        }
+
+    }
+}
